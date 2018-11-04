49d662f05c309a4f5f3348680c63f57c90fcfc0c
sun5hine <af5kh@yahoo.com>
2018-11-04 12:37:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,10 @@
-class Song 
-  
-    attr_accessor :name 
-
-def initialize(name)
-    @name = name  
-  end
-  
+class Artist
+  attr_accessor :name, :genre
  
-end 
-
+  def initialize(name, genre) 
+    @name = name
+    @genre = genre
+  end
+ 
+end
 
